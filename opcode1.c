925f008be7d27398faa5016fdc4215a24241f464
samsonkimani <samsonkimani43@gmail.com>
2022-12-15 14:35:12 +0300

    a function that divides the second top element with the top element

--- opcode1.c
+++ opcode1.c
@@ -64,3 +64,40 @@ void _sub(stack_t **head, unsigned int cline)
 	current->n -= (*head)->n;
 	_pop(head, cline);
 }
+
+/**
+ * _div - divides the second element by the top element of the stack
+ *
+ * @doubly: head of the linked list
+ * @cline: line number;
+ * Return: no return
+ */
+
+void _div(stack_t **doubly, unsigned int cline)
+{
+	int m = 0;
+	stack_t *aux = NULL;
+
+	aux = *doubly;
+
+	for (; aux != NULL; aux = aux->next, m++)
+		;
+
+	if (m < 2)
+	{
+		dprintf(2, "L%u: can't div, stack too short\n", cline);
+		free_vglo();
+		exit(EXIT_FAILURE);
+	}
+
+	if ((*doubly)->n == 0)
+	{
+		dprintf(2, "L%u: division by zero\n", cline);
+		free_vglo();
+		exit(EXIT_FAILURE);
+	}
+
+	aux = (*doubly)->next;
+	aux->n /= (*doubly)->n;
+	_pop(doubly, cline);
+}
