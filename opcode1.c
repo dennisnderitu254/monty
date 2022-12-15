acebfa806c5310a71b39292148e7728c4b87a226
samsonkimani <samsonkimani43@gmail.com>
2022-12-15 11:58:42 +0300

    creating the nop function. it does nothing

--- opcode1.c
+++ opcode1.c
@@ -27,3 +27,13 @@ void _add(stack_t **doubly, unsigned int cline)
 	_pop(doubly, cline);
 }
 
+/**
+ * _nop - doesn't do anything
+ * @doubly: linked list head
+ * @cline: line number
+ */
+void _nop(stack_t **doubly, unsigned int cline)
+{
+	(void)doubly;
+	(void)cline;
+}
