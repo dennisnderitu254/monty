c1db50657ed7d1a25ad809e1c6bd05c69405b5e3
samsonkimani <samsonkimani43@gmail.com>
2022-12-15 16:06:51 +0300

    creating a function to print the character and strings from stacks

--- get_opcodes.c
+++ get_opcodes.c
@@ -21,6 +21,8 @@ void (*get_opcodes(char *opc))(stack_t **stack, unsigned int line_number)
 		{"div", _div},
 		{"mul", _mul},
 		{"mod", _mod},
+		{"pchar", _pchar},
+		{"pstr", _pstr},
 		{NULL, NULL}
 	};
 	int i;
