5462a3c62f5b6eaff8009f64d975c97fc96c2833
samsonkimani <samsonkimani43@gmail.com>
2022-12-15 17:05:15 +0300

    a function that places the bottom element to be the top

--- opcode3.c
+++ opcode3.c
@@ -29,3 +29,32 @@ void _rotl(stack_t **doubly, unsigned int cline)
 	(*doubly)->prev = aux2;
 	*doubly = aux1;
 }
+
+/**
+ * _rotr - reverse stack
+ * @doubly: linked list head
+ * @cline: line number
+ */
+void _rotr(stack_t **doubly, unsigned int cline)
+{
+	stack_t *aux = NULL;
+	(void)cline;
+
+	if (*doubly == NULL)
+		return;
+
+	if ((*doubly)->next == NULL)
+		return;
+
+	aux = *doubly;
+
+	for (; aux->next != NULL; aux = aux->next)
+		;
+
+	aux->prev->next = NULL;
+	aux->next = *doubly;
+	aux->prev = NULL;
+	(*doubly)->prev = aux;
+	*doubly = aux;
+}
+
